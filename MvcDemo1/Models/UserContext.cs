279d6f193604e423928b5c684c0aed5e03e1eaab
Daniel Jaller <danieljaller@gmail.com>
2017-01-25 11:18:37 +0100

    Added UserContext and changed startup-controller

---
+++ MvcDemo1/Models/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo1.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<UserModel> UserModels { get; set; }
+    }
+}
